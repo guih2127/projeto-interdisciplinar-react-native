0d919107f1f4abd4dd4eab36aff974cd48a139c5
Guilherme Reis Araújo <guilherme.reis@sys10.me>
2020-05-10 17:00:52 -0300

    animações na tela de login

--- App.js
+++ App.js
@@ -1,16 +1,73 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Text } from 'react-native';
+import React, { useState, useEffect, Component } from 'react';
+import { 
+  StyleSheet, 
+  View, 
+  KeyboardAvoidingView, 
+  Image, 
+  TextInput, 
+  TouchableOpacity, 
+  Text, 
+  Animated,
+  Keyboard 
+} from 'react-native';
+
 import Header from './components/Header';
 
-class App extends Component {
-  render() {
-    const title = 'Projeto Interdisciplinar';
+export default function App() {
+
+  const [offset] = useState(new Animated.ValueXY({x: 0, y: 80}));
+  const [opacity] = useState(new Animated.Value(0));
+  const [logo] = useState(new Animated.ValueXY({x: 250, y: 250}));
+
+  useEffect(() => {
+    keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', keyboardDidShow);
+    keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', keyboardDidHide);
+
+    Animated.parallel([
+      Animated.spring(offset.y, {
+        toValue: 0,
+        speed: 4,
+        bounciness: 30
+      }),
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 200,
+      })
+    ]).start();
+  }, []);
+
+  function keyboardDidShow() {
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 150,
+        duration: 100
+      }),
+      Animated.timing(logo.y, {
+        toValue: 150,
+        duration: 100
+      }),
+    ]).start();
+  }
+
+  function keyboardDidHide() {
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 250,
+        duration: 100
+      }),
+      Animated.timing(logo.y, {
+        toValue: 250,
+        duration: 100
+      }),
+    ]).start();
+  }
+
     return (
       <KeyboardAvoidingView style={styles.background}>
         <View style={styles.containerLogo}>
-          <Image style={styles.image} source={require("./assets/logo.png")} />
+          <Animated.Image style={{width: logo.x, height: logo.y}} source={require("./assets/logo.png")} />
         </View>
-        <View style={styles.container}>
+        <Animated.View style={[styles.container, { opacity: opacity, transform: [ {translateY: offset.y } ]}]}>
           <TextInput style={styles.input} placeholder="Email" autoCorrect={false} onChangeText={() => {}} />
           <TextInput style={styles.input} placeholder="Senha" autoCorrect={false} onChangeText={() => {}} />
 
@@ -21,10 +78,9 @@ class App extends Component {
           <TouchableOpacity style={styles.btnRegister}>
             <Text style={styles.submitText}>Criar conta gratuita</Text>
           </TouchableOpacity>
-        </View>
+        </Animated.View>
       </KeyboardAvoidingView>
     );
-  }
 }
 
 const styles = StyleSheet.create({
@@ -77,7 +133,6 @@ const styles = StyleSheet.create({
   submitText: {
      color: "#FFF",
      fontSize: 18
-  }
-});
+  },
 
-export default App;
+});
